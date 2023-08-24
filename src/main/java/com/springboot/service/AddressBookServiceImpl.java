d9cead98f935d44f378316616aae2e9dd7286ef8
pk65arya <pk65arya@gmail.com>
2023-08-24 11:11:08 +0530

    Added:Store AddressBook in List

--- src/main/java/com/springboot/service/AddressBookServiceImpl.java
+++ src/main/java/com/springboot/service/AddressBookServiceImpl.java
@@ -9,36 +9,45 @@ import java.util.List;
 
 @Service
 public class AddressBookServiceImpl implements IAddressBookService {
+    List<PersonDetails> personDetailsList=new ArrayList<>();
     @Override
     public List<PersonDetails> getAddressbookData() {
-        List<PersonDetails> addressbookDataList = new ArrayList<>();
-        addressbookDataList.add(new PersonDetails(1, new AddressBookDTO("Kishlay", "7057082582")));
-        return addressbookDataList;
+
+        return personDetailsList;
     }
 
     @Override
     public PersonDetails getAddressbookDataById(int personId) {
-        PersonDetails personDetails = null;
-        personDetails = new PersonDetails(personId, new AddressBookDTO("Kishan", "1234567890"));
+       PersonDetails personDetails=null;
+       personDetails=personDetailsList.get(personId -1);
         return personDetails;
     }
 
     @Override
     public PersonDetails createAddressbookData(AddressBookDTO addressbookDTO) {
         PersonDetails personDetails = null;
-        personDetails = new PersonDetails(1,addressbookDTO);
+       personDetails=new PersonDetails(personDetailsList.size() +1,addressbookDTO);
+       personDetailsList.add(personDetails);
         return personDetails;
     }
 
     @Override
     public PersonDetails updateAddressbookData(int personId, AddressBookDTO addressbookDTO) {
-        PersonDetails personDetails = null;
-        personDetails = new PersonDetails(personId, addressbookDTO);
+
+       PersonDetails personDetails=this.getAddressbookDataById(personId);
+       personDetails.setName(addressbookDTO.name);
+       personDetails.setPhNumber(addressbookDTO.phNumber);
+       personDetailsList.set(personId -1,personDetails);
         return personDetails;
     }
 
     @Override
     public void deleteAddressbookData(int personId) {
+        int i=1;
+        personDetailsList.remove(personId -1);
+        for (PersonDetails personDetails:personDetailsList){
+            personDetails.setPersonId(i++);
+        }
 
     }
 }
