737dca6ed078f2776549ab111afeab115875e1ed
pk65arya <pk65arya@gmail.com>
2023-08-24 11:31:24 +0530

    Added:provided user friendly error

---
+++ src/main/java/com/springboot/exception/AddressBookExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.springboot.exception;
+
+import com.springboot.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class AddressBookExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<ObjectError> errorList=exception.getBindingResult().getAllErrors();
+        List<String> errmsg=errorList.stream().map(objectError -> objectError.getDefaultMessage()).collect(Collectors.toList());
+        ResponseDTO responseDTO=new ResponseDTO("Exception Whilr processing Rest Request",errmsg);
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
